ba8c24c571509080c07cac7d3d54d78497cbae8e
Andre Silva <andreswebs@pm.me>
2022-01-19 20:47:37 -0300

    update locals

--- main.tf
+++ main.tf
@@ -21,7 +21,8 @@ data "aws_region" "current" {}
 data "aws_partition" "current" {}
 
 locals {
-  secret_arns = [for s in var.secret_names : "arn:${data.aws_partition.current.partition}:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret:${s}-??????"]
+  secret_arn_prefix = "arn:${data.aws_partition.current.partition}:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret"
+  secret_arns = [for s in var.secret_names : "${local.secret_arn_prefix}:${s}-??????"]
 }
 
 data "aws_iam_policy_document" "this" {
