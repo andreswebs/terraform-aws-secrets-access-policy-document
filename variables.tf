f04b7dc8e693304f222ddb40dddaf2b2a900f222
Keshav Kaul <keshavkaul@users.noreply.github.com>
2022-02-11 18:35:35 +0530

    create additional_actions variable

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "secret_names" {
   type        = list(string)
   description = "List of friendly names of the allowed secrets"
 }
+
+variable "additional_actions" {
+  type        = list(string)
+  description = "List of additional policy actions for the allowed secrets"
+}
